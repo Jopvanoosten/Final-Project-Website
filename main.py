f24b2d077a5ede6c190fdc458333a16932b5eab3
Jop <jopvanoosten@gmail.com>
2026-07-02 23:09:25 +0000

    update voor render (flask mogelijkheid)

--- main.py
+++ main.py
@@ -207,4 +207,6 @@ if __name__ == "__main__":
     # Initialiseer de database wanneer de applicatie start
     # Dit zorgt ervoor dat de 'spots' tabel wordt aangemaakt als deze nog niet bestaat
     init_db()
-    app.run(host='0.0.0.0', port=5001, debug=True) # debug=True is handig voor ontwikkeling
+    port = int(os.environ.get('PORT', 5001))
+    debug = os.environ.get('FLASK_DEBUG', '0') == '1'
+    app.run(host='0.0.0.0', port=port, debug=debug)
